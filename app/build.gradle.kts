f99428dbae35b562a0dbc6db30959d4c391a91bc
ijaejin <dlwowls04@swu.ac.kr>
2026-07-29 22:34:49 +0900

    카카오 API 키 숨김

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,6 @@ import java.util.Properties
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    // alias(libs.plugins.kotlin.compose) (재진)
     id("kotlin-kapt")
 
     id("com.google.devtools.ksp")
